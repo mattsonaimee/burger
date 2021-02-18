0f2b622b358850203bed1a02e3f428731e5df265
amatt <amattson84@gmail.com>
2021-02-17 22:20:33 -0800

    Corrected syntax error

--- config/connection.js
+++ config/connection.js
@@ -10,5 +10,5 @@ database: 'burgers_db',
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log( `Line 13 connection.js: connected as id ${connection.threadId}`)
+    console.log( `Line 13 connection.js: connected as id ${connection.threadId}`);
 });
