88b6f92ac816df54a860b36dbb4fe51d37862799
amatt <amattson84@gmail.com>
2021-02-18 12:25:03 -0800

    Changed method name from update to updateOne

--- config/orm.js
+++ config/orm.js
@@ -56,7 +56,7 @@ var orm = {
     });
   },
 
-  update: function (table, objColVals, condition, cb) {
+  updateOne: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
     queryString += " SET ";
