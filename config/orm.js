b7649efd917257b824904ac7586db925ed884960
amatt <amattson84@gmail.com>
2021-02-18 11:59:49 -0800

    updated BOOLEAN values from True to False

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,79 @@
-const connection = require('./connection.js');
+const connection = require("./connection.js");
+
+function questionMarks(num) {
+  var arr = [];
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+function objectToSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+
+}
+
+var orm = {
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELCT * FROM" + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        console.log("error line 31 orm.js");
+        throw err;       
+      }
+     
+      cb(result);
+    });
+  },
+
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO" + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += questionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        console.log("error line 52 orm.js");
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  update: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objectToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        console.log("error line 70 orm.js");
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
