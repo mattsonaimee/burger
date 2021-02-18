2161cbb2b3377fe4f995868c9a3bee0eaab9fd63
amatt <amattson84@gmail.com>
2021-02-18 12:25:38 -0800

    created burger object and exported it

--- models/burgers.js
+++ models/burgers.js
@@ -1,1 +1,23 @@
 const orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+  
+    insertOne: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateone: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+
+  module.exports = burger;
