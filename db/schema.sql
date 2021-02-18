68593c9405e7441969536fa88d9b66e3046cabd6
amatt <amattson84@gmail.com>
2021-02-17 22:05:31 -0800

    added value of 30 to id integer

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INTEGER() AUTO_INCREMENT NOT NULL,
+    id INTEGER(30) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR (30) NOT NULL,
     devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
