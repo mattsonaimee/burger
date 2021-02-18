ea7dd5c65110322c35c015048fa1581507827885
amatt <amattson84@gmail.com>
2021-02-18 14:59:56 -0800

    Added process.env PORT

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
