1ada638f916efcc8ec8da3f270d312fec578ae30
amatt <amattson84@gmail.com>
2021-02-17 20:16:28 -0800

    seeds.sql. 4 entries added to burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES(1, 'Cheese burger', TRUE);
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES(2, 'Veggie burger', FALSE);
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES(3, 'Bacon burger', TRUE);
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES(4, 'Monster burger', FALSE);
