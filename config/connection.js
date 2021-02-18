038cff43434013b99b01add07cc49d3a0e94be1b
amatt <amattson84@gmail.com>
2021-02-17 22:15:56 -0800

    required and created connection to mysql

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+host: 'localhost',
+port: 3306,
+user: 'root',
+password: '#Nachos11',
+database: 'burgers_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log( `Line 13 connection.js: connected as id ${connection.threadId}`)
+});
