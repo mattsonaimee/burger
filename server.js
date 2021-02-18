10914c719f0467a10ea68050ebb01d4ad1e7d601
amatt <amattson84@gmail.com>
2021-02-18 15:19:23 -0800

    Changed name of file

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
-const mysql = require('mysql');
-
-const app = express();
-
 
 const PORT = process.env.PORT || 8080;
+var app = express();
 
-
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+
+
+
+
+
 
 app.listen(PORT, () =>
   console.log(`Server listening on: http://localhost:${PORT}`)
 );
+
