6bac028166614cb13e9474992490660a8ff4c49e
amatt <amattson84@gmail.com>
2021-02-18 13:08:40 -0800

    Wrote .get and .post methods

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,3 +3,19 @@ var express = require("express");
 var router = express.Router();
 
 var burger = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+    burger.all((data) => {
+            var brgrObject = {
+                burgers: data
+            };
+            console.log(brgrObject);
+            res.render("index", brgrObject);
+        });
+    });
+
+    router.post("/api/burgers", (req, res) => {
+        burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], (result) =>{
+            res.json({ id: result.insertId});
+        });
+    });
