e70a44f20be1a33cb95e57511736a3de08cd6315
amatt <amattson84@gmail.com>
2021-02-18 19:48:58 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,23 @@
-const mysql = require('mysql');
-
+const mysql = require("mysql");
+var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: '#Nachos11',
-database: 'burgers_db',
-});
-};
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "#Nachos11",
+    database: "burgers_db",
+  });
+}
 
 connection.connect((err) => {
-    if (err) { 
-        console.error(`Line 13 connection.js. error connecting: ${err.stack}`);
-        return;
-    }       
-    console.log( `Line 16 connection.js: connected as id ${connection.threadId}`);
+  if (err) {
+    console.error(`Line 13 connection.js. error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`Line 16 connection.js: connected as id ${connection.threadId}`);
 });
 
 module.exports = connection;
