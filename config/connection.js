2e7a14c4c6d6aa9f47f531353556909c8fcde62e
amatt <amattson84@gmail.com>
2021-02-18 18:26:25 -0800

    removed port element 3306

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,10 @@ const mysql = require('mysql');
 
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.send.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-const connection = mysql.createConnection({
+connection = mysql.createConnection({
 host: 'localhost',
-port: 3306,
 user: 'root',
 password: '#Nachos11',
 database: 'burgers_db',
