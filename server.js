54740a49781dad94cc101d3c9594bbf852a10f67
amatt <amattson84@gmail.com>
2021-02-20 11:27:40 -0800

    changed const to var

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 
 app.use(express.static("public"));
